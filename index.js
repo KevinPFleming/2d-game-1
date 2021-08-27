8f2e8a76eb05ed49703b37ab472e4f0219dd9a74
kevinpfleming <kevin.patrick.fleming@gmail.com>
2021-08-27 13:35:28 -0400

    player-creation

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ canvas.width = 800;
 canvas.height = 500;
 ctx.font = '50px Georgia';
 
-let score = 0;
 // the gameFrame variable Sets up Animation Loop to spawn new animated items
+let score = 0;
 let gameFrame = 0;
 
 // Mouse Interactivity
@@ -19,9 +19,68 @@ const mouse = {
 }
 
 // measures x and y distances from browser and not canvas
+// x and y override the current mouse position
 canvas.addEventListener('mousedown', function(event) {
+  mouse.click = true;
   mouse.x = event.x - canvasPosition.left;
   mouse.y = event.y - canvasPosition.top;
-  console.log(event);
 })
+
+canvas.addEventListener('mouseup', function(){
+  mouse.click = false;
+})
+
+// Player Creation
+class Player {
+  constructor(){
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
+    this.radius = 50;
+    this.angle = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.frame = 0;
+    this.spriteWidth = 498;
+    this.spriteHeight = 327;
+
+  }
+  update(){
+    const dx = this.x - mouse.x;
+    const dy = this.y - mouse.y;
+    if(mouse.x != this.x){
+      this.x -= dx/30;
+    }
+    if(mouse.y != this.y){
+      this.y -= dy/30;
+    }
+  }
+  draw(){
+    if(mouse.click) {
+      ctx.lineWidth = 0.2;
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.stroke();
+    }
+    ctx.fillStyle = 'green';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const player = new Player();
+
+// Create Animation Loop
+function animate(){
+  // clears the canvas animation trails each time the mouse is engaged
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+  player.draw();
+  // Creates an animation loop through recursion(Calling the function continuously)
+  requestAnimationFrame(animate);
+}
+
+animate();
 
